b6def1a31d676637a59e860e2cb9e96bd094b9cd
wanto <wantoine@outlook.fr>
2022-02-06 21:56:26 +0100

    connection user

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,41 @@
-console.log("test")
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
-var socket = io("http://localhost:3000");
+let socket;
+let connectedUsers = [];
 
-var text = document.getElementById('text');
+docReady(() => {
+    console.log("test")
 
-socket.on("welcome", (msg) => {
-    text.innerText = msg;
+    socket = io("http://localhost:3000");
+    let text = document.getElementById('text');
+
+    socket.on("welcome", (msg) => {
+        text.innerText = msg;
+    })
+
+    socket.on('Error', (data) =>{
+        text.style.color = 'red';
+        text.innerText = data;
+    })
+
+    socket.on("listConnectedUsers", (list) =>{
+        connectedUsers = list;
+        console.log(connectedUsers);
+    })
 })
+
+function sendPacket(eventName,data){
+    socket.emit(eventName,data);
+}
+
+function loginFunction() {
+    sendPacket('userConnect', document.getElementById('pseudo').value);
+}
--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let listUser = [];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -16,7 +17,40 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.emit('welcome', "bienvenue sur le serveur !!");
+
+    socket.on('userConnect', (data) => {
+        console.log("connecttion of "+data);
+
+        for (let user of listUser){
+
+            if (user.name.toLowerCase() == data.toLowerCase()){
+                socket.emit('Error',data+' already taken');
+                return;
+            }
+
+        }
+
+        let user = {
+            name: data,
+            socket: socket
+        }
+        listUser.push(user);
+        sendConnectedUsers();
+    });
+
 });
+
+function sendConnectedUsers(){
+
+    let listConnectedUsers = [];
+
+    for (let user of listUser){
+        listConnectedUsers.push(user.name);
+    }
+
+    io.emit('listConnectedUsers',listConnectedUsers);
+
+}
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
